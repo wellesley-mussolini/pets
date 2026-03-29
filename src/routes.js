633670188324b8f8f08e33a081bcde27ba2b4668
wellesley-corpcanal <wellesley@corpcanal.com.br>
2026-03-29 19:06:52 -0300

    limpeza de repositorio

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-const multer = require('multer');
-const multerConfig = require('./config/multer');
-
-const FileController = require('./controllers/FileController');
-
-routes.post('/files', multer(multerConfig).single('file'), FileController.create);
-
-module.exports = routes; 
