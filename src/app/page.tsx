7419a39101ceface9a7d0f060f1f0f409c3bbc5f
wellesley-corpcanal <wellesley@corpcanal.com.br>
2026-03-29 22:13:32 -0300

    feat: initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
 import { DogSpinningCircle } from "../components/loading/dog-spinning-circle.component";
 import { DogWagTail } from "../components/loading/dog-wag-tail.component";
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center flex-col h-screen">
       <DogWagTail />
       <DogSpinningCircle />
+      <Button
+        variant="outline"
+        onClick={() =>
+          toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+      >
+        Show Toast
+      </Button>
     </div>
   );
 }
